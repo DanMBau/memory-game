d1b80a7a810762a1c448ba91556b2e94a53160c7
DocHolidayDeluxe <dabauer@me.com>
2019-06-22 13:25:24 -0400

    logic fully working

--- src/App.js
+++ src/App.js
@@ -13,76 +13,66 @@ class App extends Component {
     correctGuesses: 0,
     bestScore: 0,
     heading: "",
-    clicked: false,
-};
+    count: [],
+  };
 
 
 
-handleIncrement = (id) => {
-  this.setState({ clicked: true });
-  
-  if (this.state.clicked) {
-    this.setState({ correctGuesses: this.state.correctGuesses + 1, heading: "You guessed correctly!" });
+  handleClick = (id) => {
 
-    if (this.state.correctGuesses > this.state.bestScore) {
-      this.setState({ bestScore: this.state.correctGuesses });
-    this.setState({correctGuesses: 0, heading: "You guessed incorrectly!" });
+    if (this.state.count.includes(id)) {
+      // you lose
+      this.setState({
+        correctGuesses: 0,
+        heading: "You guessed incorrectly!",
+        count: []
+      });
 
-    }
-    
-  } else {
-    // this.setState({correctGuesses: 0, heading: "You guessed incorrectly!" });
-    /* console.log(this.state.idsClicked); */
-  }
-  
-};
-
-  setClicked = id =>{
-
-    //state pics array
-    let pics = this.state.pics;
-    
-    
-
+    } else {
+      // game continues
+      let pics = [...this.state.pics].sort(function (a, b) { return 0.5 - Math.random() });
+      console.log('corrGuesses', this.state.correctGuesses);
       
-      //update with random array
-      
-      this.handleIncrement()
-      pics.sort(function(a, b){return 0.5 - Math.random()});
       this.setState({ pics });
-      
+
+      this.setState({
+        count: [...this.state.count, id],
+        heading: "You guessed correctly!",
+        correctGuesses: this.state.correctGuesses + 1,
+
+      });
+      console.log('corrGuesses', this.state.correctGuesses);
+      if (this.state.correctGuesses >= this.state.bestScore) {
+        this.setState({
+          bestScore: this.state.correctGuesses + 1
+        });
+
+      }
     }
-
-    
-
-
-
+  }
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <>
         <Navbar
-        correctGuesses={this.state.correctGuesses}
-        bestScore={this.state.bestScore}
-        heading={this.state.heading || "Click an image to begin!"}/>
+          correctGuesses={this.state.correctGuesses}
+          bestScore={this.state.bestScore}
+          heading={this.state.heading || "Click an image to begin!"} />
         <div className="container">
           <div className="row">
-        {this.state.pics.map(picture => (
-          <Picture 
-            setClicked={this.setClicked}
-            id={picture.id}
-            key={picture.id}
-            image={picture.image}
-            correctGuesses={this.state.correctGuesses}
-            handleIncrement={this.handleIncrement}
-						handleHighestScore={this.handleHighestScore}
-          />
-          ))}
+            {this.state.pics.map(picture => (
+              <Picture
+                id={picture.id}
+                key={picture.id}
+                image={picture.image}
+                handleClick={this.handleClick}
+              />
+            ))}
           </div>
-          </div>
+        </div>
       </>
-    );
+    )
   };
 }
 
